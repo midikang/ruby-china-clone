2687c64b4a76ee3d3f774fd1f8e9bcdc9331b569
Midi <midi13@163.com>
2016-08-05 04:33:49 +0000

    1fc1936| complete cpanel

--- app/controllers/cpanel/replies_controller.rb
+++ app/controllers/cpanel/replies_controller.rb
@@ -28,7 +28,7 @@ class Cpanel::RepliesController < Cpanel::ApplicationController
 
     respond_to do |format|
       if @reply.save
-        format.html { redirect_to @reply, notice: 'Reply was successfully created.' }
+        format.html { redirect_to cpanel_replies_path, notice: 'Reply was successfully created.' }
         format.json { render :show, status: :created, location: @reply }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class Cpanel::RepliesController < Cpanel::ApplicationController
   def update
     respond_to do |format|
       if @reply.update(reply_params)
-        format.html { redirect_to @reply, notice: 'Reply was successfully updated.' }
+        format.html { redirect_to cpanel_replies_path, notice: 'Reply was successfully updated.' }
         format.json { render :show, status: :ok, location: @reply }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class Cpanel::RepliesController < Cpanel::ApplicationController
   def destroy
     @reply.destroy
     respond_to do |format|
-      format.html { redirect_to replies_url, notice: 'Reply was successfully destroyed.' }
+      format.html { redirect_to cpanel_replies_path, notice: 'Reply was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/views/cpanel/replies/_reply.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! reply, :id, :topic_id, :body, :user_id, :state, :source, :created_at, :updated_at
-json.url reply_url(reply, format: :json)
